ea6bbb79e4dbf8c571b4a276e7475b663d4598c8
JanAnders <ja045@hdm-stuttgart.de>
2018-05-08 19:24:36 +0200

    Added 3 Classes BO, ContactList and Contact

---
+++ ITProject_Group11_Jabics/src/de/hdm/group11/jabics/shared/bo/Contact.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package de.hdm.group11.jabics.shared.bo;
+
+/**
+ * @author Anders
+ *
+ */
+
+public class Contact extends BusinessObject {
+
+	//Code will be added soon
+}
---
+++ ITProject_Group11_Jabics/src/de/hdm/group11/jabics/shared/bo/ContactList.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package de.hdm.group11.jabics.shared.bo;
+
+/**
+ * @author Jan
+ *
+ */
+public class ContactList extends BusinessObject {
+
+}
