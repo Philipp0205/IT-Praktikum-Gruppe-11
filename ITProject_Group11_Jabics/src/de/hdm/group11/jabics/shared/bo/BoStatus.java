1a6f927b2cbebb19cf35b5d0e986eac4b6b90480
Philipp <philipp.kurrle@gmail.com>
2018-05-09 16:56:53 +0200

    Added more methods.

--- ITProject_Group11_Jabics/src/de/hdm/group11/jabics/shared/bo/BoStatus.java
+++ ITProject_Group11_Jabics/src/de/hdm/group11/jabics/shared/bo/BoStatus.java
@@ -1,4 +1,4 @@
-
+package de.hdm.group11.jabics.shared.bo;
 public enum BoStatus {
 	IS_SHARED, 
 	PARTIALLY_SHARED,
--- ITProject_Group11_Jabics/src/de/hdm/group11/jabics/shared/bo/PValue.java
+++ ITProject_Group11_Jabics/src/de/hdm/group11/jabics/shared/bo/PValue.java
@@ -1,4 +1,55 @@
+package de.hdm.group11.jabics.shared.bo;
+
+import java.util.Date;
 
 public class PValue {
+	
+	private int intValue; 
+	private String stringValue; 
+	private Date dateValue; 
+	private float floatValue; 
+	private Property property;
+	
+	public String toString() {
+		return stringValue; 
+		/**
+		 * TODO
+		 */
+	}
+	
+	
+	/** 
+	 * Getter uns Setter 
+	 */
+	public int getIntValue() {
+		return intValue;
+	}
+	public void setIntValue(int intValue) {
+		this.intValue = intValue;
+	}
+	public String getStringValue() {
+		return stringValue;
+	}
+	public void setStringValue(String stringValue) {
+		this.stringValue = stringValue;
+	}
+	public Date getDateValue() {
+		return dateValue;
+	}
+	public void setDateValue(Date dateValue) {
+		this.dateValue = dateValue;
+	}
+	public float getFloatValue() {
+		return floatValue;
+	}
+	public void setFloatValue(float floatValue) {
+		this.floatValue = floatValue;
+	}
+	public Property getProperty() {
+		return property;
+	}
+	public void setProperty(Property property) {
+		this.property = property;
+	}
 
 }
