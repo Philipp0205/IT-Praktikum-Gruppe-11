db483fd8424036b7a2a46796743163f466b835a1
Philipp <philipp.kurrle@gmail.com>
2018-05-09 16:35:28 +0200

    Added methods.

--- ITProject_Group11_Jabics/src/de/hdm/group11/jabics/shared/bo/ContactList.java
+++ ITProject_Group11_Jabics/src/de/hdm/group11/jabics/shared/bo/ContactList.java
@@ -2,6 +2,7 @@
  * 
  */
 package de.hdm.group11.jabics.shared.bo;
+import java.util.ArrayList;
 
 /**
  * @author Anders
@@ -9,12 +10,48 @@ package de.hdm.group11.jabics.shared.bo;
  */
 public class ContactList extends BusinessObject {
 	
-			BoStatus bo = BoStatus.
 	
-			//Code to be added
 			private String listName; 
 			//private BoStatus shareStatus; 
-			ArrayList<Conatct> contacts = new ArrayList<Contact>();  
+			ArrayList<Contact> contacts = new ArrayList<Contact>();  
+			BoStatus shareStatus;
+			
+			public String toString() {
+				
+				/** 
+				 * TODO 
+				 */
+				return listName; 
+				
+			}
+
+			public void addContact(Contact c) { 
+				/**
+				 *  TODO implementieren 
+				 */
+				
+				contacts.add(c);
+			}
+			
+			public void removeContact(Contact c) {
+				/** 
+				 *  TODO 
+				 */
+			}
+			// Getter und Setter 
+			
+			public String getListName() {
+				return listName;
+			}
+			public void setListName(String listName) {
+				this.listName = listName;
+			}
+			public BoStatus getShareStatus() {
+				return shareStatus;
+			}
+			public void setShareStatus(BoStatus shareStatus) {
+				this.shareStatus = shareStatus;
+			}
 			
 			
 }
