3862f20196f5c036b5002088b33b1ee62a21209c
Philipp <philipp.kurrle@gmail.com>
2018-05-11 16:50:26 +0200

    Fixes merge conflicts.

---
+++ ITProject_Group11_Jabics/src/de/hdm/group11/jabics/server/ReportGeneratorServiceImpl.java
@@ -0,0 +1,9 @@
+package de.hdm.group11.jabics.server;
+
+import com.google.gwt.user.server.rpc.*;
+
+public class ReportGeneratorServiceImpl extends RemoteServiceServlet{
+	/**
+	 * TODO: implement all methods
+	 */
+}
