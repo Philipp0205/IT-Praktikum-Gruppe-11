e99ffbeba68287253ec6177802ae93842b96892b
JanAnders <ja045@hdm-stuttgart.de>
2018-05-13 20:22:54 +0200

    Added Methods in EditorServiceImpl

--- ITProject_Group11_Jabics/src/de/hdm/group11/jabics/server/EditorServiceImpl.java
+++ ITProject_Group11_Jabics/src/de/hdm/group11/jabics/server/EditorServiceImpl.java
@@ -5,22 +5,20 @@ package de.hdm.group11.jabics.server;
 
 import java.util.ArrayList;
 
-import de.hdm.group11.jabics.server.db.ContactListMapper;
-import de.hdm.group11.jabics.server.db.ContactMapper;
-import de.hdm.group11.jabics.server.db.PValueMapper;
-import de.hdm.group11.jabics.server.db.PropertyMapper;
-import de.hdm.group11.jabics.shared.bo.Contact;
-import de.hdm.group11.jabics.shared.bo.ContactList;
-import de.hdm.group11.jabics.shared.bo.PValue;
-import de.hdm.group11.jabics.shared.bo.Property;
-import de.hdm.group11.jabics.shared.bo.User;
-import de.hdm.group11.jabics.shared.bo.Type;
+import de.hdm.group11.jabics.server.db.*;
+import de.hdm.group11.jabics.shared.bo.*;
+/**
+ * TODO: write interface EditorService
+ */
+import de.hdm.group11.jabics.shared.EditorService;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
 /**
  * @author Jan
  *
  */
-public class EditorServiceImpl extends EditorService {
+public class EditorServiceImpl extends RemoteServiceServlet implements EditorService{
 	
 	/**
 	 * TODO: implement all methods
@@ -30,7 +28,7 @@ public class EditorServiceImpl extends EditorService {
 	ContactListMapper clMapper = new ContactListMapper();
 	PValueMapper pvMapper = new PValueMapper();
 	PropertyMapper pMapper = new PropertyMapper();
-	// UserMapperImpl uMapper = new UserMapperImpl();
+	UserMapper uMapper = new UserMapper();
 	
 	
 	public EditorServiceImpl() {
@@ -38,34 +36,39 @@ public class EditorServiceImpl extends EditorService {
 	}
 	
 	public User createUser(String name) { 
-		User newUser = new User();
-		newUser.setUsername(name);
-		
-		return newUser;
+		User newUser = new User(name);
+		return uMapper.createUser(newUser);
 	}
 	
-	public Contact createContact(ArrayList<PValue> cArray ) { 
-		Contact newContact = new Contact();
-		newContact.setValues(cArray);
-		
-		return newContact;
+	public Contact createContact(ArrayList<PValue> cArray) { 
+		Contact newContact = new Contact(cArray);
+		return cMapper.insertContact(newContact);
 	}
 	
 	public ContactList createContactList(ArrayList<Contact> cArray) { 
-		ContactList newContactList = new ContactList();
-		newContactList.setContacts(cArray);
-		
-		return newContactList;
+		ContactList newContactList = new ContactList(cArray);
+		return clMapper.insertContactList(newContactList);
 	}
 	
+	//Are the create PValue Methods really needed? can't this be done on clientside and we just insert these values into the database?
 	public PValue createPValue(Property p, String s, Contact c) {
 		PValue newPValue = new PValue(p, s);
 		c.addPValue(newPValue);
-		
+		//to be determined: does insert pv also connect pv with contact? if yes, next method is unnecessary
+		pvMapper.insertPValue(pv, c);
+		// potentially add user object into method parameters
+		cMapper.updateContact(c, u)
 		return newPValue; 
 		
 	}
 	
+	/**
+	 * TODO: determine if comments in method above also apply here
+	 * @param p
+	 * @param i
+	 * @param c
+	 * @return
+	 */
 	public PValue createPValue(Property p, int i, Contact c) {
 		PValue newPValue = new PValue(p, i);
 		c.addPValue(newPValue);
@@ -76,7 +79,7 @@ public class EditorServiceImpl extends EditorService {
 	
 	// TODO DOKU: Statt "name" label und die überladene Methode
 	public Property createProperty(String label) {
-		Property newProperty = new Property();
+		Property newProperty = new Property(label);
 		newProperty.setLabel(label);
 		
 		return newProperty;
@@ -100,11 +103,103 @@ public class EditorServiceImpl extends EditorService {
 		return null; 
 	}
 	
-	// TODO
+	// is this method really needed?
 	public User getUSerById(int id) {
-		return null;
+		return uMapper.findUserById(id);
 	}
 	
+	/**
+	 * This Method inserts a specified <code>Contact</code> into a list
+	 * @param c Contact
+	 * @param cl ContactList
+	 * @return updated contact list
+	 */
+	public ContactList addContactToList(Contact c, ContactList cl) {
+		cl.addContact(c);
+		return clMapper.updateContactList(cl);
+		
+	}
+	
+	public Contact addValueToContact(PValue pv, Contact c, User u) {
+		c.addPValue(pv);
+		return cMapper.updateContact(c, u);
+	}
+	
+	/**
+	 * Searches a <code>ContactList</code> for all <code>Contact</code>s that contain the passed on String
+	 * @param cl ContactList that shall be searched
+	 * @param s Expression that shall be searched for
+	 * @return ArrayList with all Contacts that fit the criteria
+	 */
+	public ArrayList<Contact> searchInList(ContactList cl, String s){
+		ArrayList<Contact> result = new ArrayList<Contact>();
+		for(Contact c : cl.getContacts()) {
+			if(c.getName().contains(s)) result.add(c);
+			for (PValue pv : c.getValues()) {
+				/**
+				 * TODO: add more fields that are searched through
+				 */
+				if (pv.getProperty().toString().contains(s) || pv.getStringValue().contains(s)) {
+					result.add(c);
+				}
+			}
+		}
+		return result;
+	}
+	
+	/**
+	 * Searches for all <code>Contacts</code> visible to a User which contain specified String
+	 * @param s
+	 * @param u
+	 * @return ArrayList with all found Contacts
+	 */
+	public ArrayList<Contact> searchForContactByExpression(String s, User u){
+		//creating new ContactList for all contacts of given user to be able to call already implemented method
+		ContactList cl = new ContactList(cMapper.findAllContact(u));
+		return this.searchInList(cl, s);
+		
+	}
+	/**
+	 * Removes a <code>Contact</code> from a <code>ContactList</code>
+	 * @param c
+	 * @param cl
+	 * @return updated ContactList without specified Contact
+	 */
+	public ContactList removeContactFromList(Contact c, ContactList cl) {
+		cl.removeContact(c);
+		return clMapper.updateContactList(cl);
+	}
+	
+	/**
+	 * Deletes a contact from the Database. The Contact is deleted for all Users. This deletes it permanently and cannot be undone.
+	 * @param c Contact that shall be deleted
+	 */
+	public void deleteContact(Contact c){
+		cMapper.deleteContact(c);
+	}
+	/**
+	 * Deletes a <code>ContactList</code> from the Database. Is deleted for all Users. This deletes it permanently and cannot be undone.
+	 * @param cl ContactList that shall be deleted
+	 */
+	public void deleteContactList(ContactList cl){
+		clMapper.deleteContactList(cl);
+	}
+	/**
+	 * Deletes a property from the database. The property is deleted for all users. This deletes it permanently and cannot be undone.
+	 * @param c Contact that shall be deleted
+	 */
+	public void deleteProperty(Property p){
+		if(!p.isStandard()) {
+			pMapper.deleteProperty(p);
+		}
+		else System.out.println("Tried to delete standard property. This should not have been possible");
+	}
+	
+	public void deletePValue(PValue pv){
+		if(pv.getProperty().getLabel() != "name") {
+			pvMapper.deletePValue(pv);
+		}
+	}
 	
 	
 	
