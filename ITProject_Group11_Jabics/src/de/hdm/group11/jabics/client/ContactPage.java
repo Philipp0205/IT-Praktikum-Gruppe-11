cda4390f15487bbc3f85307055ce198a00341f7e
AlexanderGerlings <33658080+AlexanderGerlings@users.noreply.github.com>
2018-05-29 15:38:44 +0200

    Update ContactPage.java

--- ITProject_Group11_Jabics/src/de/hdm/group11/jabics/client/ContactPage.java
+++ ITProject_Group11_Jabics/src/de/hdm/group11/jabics/client/ContactPage.java
@@ -1,18 +1,26 @@
 package de.hdm.group11.jabics.client;
 
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
+
 public class ContactPage extends Composite {
+	
 	private VerticalPanel vPanel = new VerticalPanel();
 	
 	public ContactPage() {
 		initWidget(this.vPanel);
 		
-		Image contactPage = new Image("/images/Unbenannt-1.png");
-		contactPage.setWidth("600px");
+		final Button findAllContacts = new Button("Alle Kontakte finden");
+		findAllContacts.setStylePrimaryName("standardButton");
 		
-		this.vPanel.add(contactPage);
-	}
+		this.vPanel.add(findAllContacts);
+		
+		final Button showContact = new Button("ausgewählten Kontakt anzeigen");
+		showContact.setStylePrimaryName("standardButton");
+		this.vPanel.add(showContact);
+		
+	}	
 }
