bd542948173dc1eab599df253c0a3e62b1e4a290
TobiasIlg <ti009@hdm-stuttgart.de>
2018-06-05 02:57:49 +0200

    BOEditor hinzugefügt
    BOEditor hinzugefügt

---
+++ ITProject_Group11_Jabics/src/de/hdm/group11/jabics/client/gui/BOEditor.java
@@ -0,0 +1,81 @@
+package de.hdm.group11.jabics.client;
+
+import java.util.ArrayList;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+import de.hdm.group11.jabics.shared.bo.PValue;
+import de.hdm.group11.jabics.shared.bo.Property;
+
+
+public class BOEditor extends Composite {
+	
+	/**
+	 *  Aufbau des Editors
+	 */
+	
+	private VerticalPanel Box = new VerticalPanel();
+
+	
+		public BOEditor(ArrayList<PValue> ary) {
+			
+			RootPanel.get("Navigator").add(Box);
+			
+			for (int i = 0; i < ary.length ; i++ ) {
+			      showProp(ary[i]);
+			    }
+			
+		}
+		
+		
+		public void showProp(Property prop) {
+			
+			/**
+			 *  Für jedes Wertepaar (Eigenschaft & Eigenschaftsausprägung) erstellt die Methode
+			 *  ein neues Element bestehend aus einem HorizontalPanel mit Text- und Input-Feld
+			 */
+			
+			private String label = prop.label;
+			private Type value = prop.type;
+			
+			final HorizontalPanel nextProperty = new HorizontalPanel();
+			Box.add(nextProperty);
+			
+			Label lbl = new Label(label);
+			TextBox tb = new TextBox(value);
+			
+			nextProperty.add(lbl);
+			nextProperty.add(tb);
+			
+			final Button editBtn = new Button("Edit");
+			final Button saveBtn = new Button("Save");
+			saveBtn.setEnabled(false); 
+			
+			/*
+			 * Switchmethode noch zu implementieren
+			 */
+			
+			nextProperty.add(editBtn);
+			nextProperty.add(saveBtn);
+			
+		}
+		
+		
+		final Button delCon = new Button("Kontakt löschen");
+		Box.add(delCon);
+		
+		/*
+		 * RPC zu Kontakt löschen
+		 */
+		
+	}
+	
+}
