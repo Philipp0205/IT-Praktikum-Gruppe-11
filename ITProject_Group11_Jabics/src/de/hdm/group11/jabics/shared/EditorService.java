2a7ffa61260ee5cbf1501591b62f7e9802cb4ad1
AlexanderStahl <stahl.alexander@live.de>
2018-07-05 20:40:39 +0200

    Member der Interfaces EditorService und EditorServiceAsync sortiert.

--- ITProject_Group11_Jabics/src/de/hdm/group11/jabics/shared/EditorService.java
+++ ITProject_Group11_Jabics/src/de/hdm/group11/jabics/shared/EditorService.java
@@ -19,94 +19,94 @@ import de.hdm.group11.jabics.shared.bo.*;
 @RemoteServiceRelativePath("editor")
 public interface EditorService extends RemoteService {
 
+	public void addCollaboration(Contact c, JabicsUser u);
+
+	public JabicsUser addCollaboration(ContactList cl, JabicsUser u);
+
+	public void addCollaboration(PValue pv, JabicsUser u);
+
+	public Contact addContactToList(Contact c, ContactList cl);
+
 	public Contact createContact(ArrayList<PValue> cArray, JabicsUser u);
 
 	public ContactList createContactList(String name, ArrayList<Contact> cArray, JabicsUser u);
 
-	public PValue createPValue(Property p, String s, Contact c, JabicsUser u);
-
-	public PValue createPValue(Property p, int i, Contact c, JabicsUser u);
+	public Property createProperty(String label, Type type);
 
 	public PValue createPValue(Property p, Date dt, Contact c, JabicsUser u);
 
 	public PValue createPValue(Property p, float i, Contact c, JabicsUser u);
-
-	public Property createProperty(String label, Type type);
-
-	public ArrayList<ContactList> getListsOf(JabicsUser u);
-
-	public ArrayList<Contact> getContactsOf(JabicsUser u);
 	
-	public Contact getUpdatedContact(Contact c);
+	public PValue createPValue(Property p, int i, Contact c, JabicsUser u);
 	
-	public ArrayList<Contact> getAllSharedContactsOf(JabicsUser u);
-
-	public Contact addContactToList(Contact c, ContactList cl);
-
-	public ArrayList<Contact> searchForContactByExpression(String s, JabicsUser u);
-
-	public Contact removeContactFromList(Contact c, ContactList cl);
-
-	public void deleteContact(Contact c, JabicsUser u);
-	
-	public ContactList deleteContactList(ContactList cl, JabicsUser u);
-	
-	public void deleteUser(JabicsUser u);
-
-	public void deleteProperty(Property p);
-
-	public void deletePValue(PValue pv, Contact c);
-
-	public PValue updatePValue(PValue pv);
-
-	public Contact updateContact(Contact c, JabicsUser u);
-
-	public ContactList updateContactList(ContactList cl);
-
-	public JabicsUser addCollaboration(ContactList cl, JabicsUser u);
-
-	public void addCollaboration(Contact c, JabicsUser u);
-
-	public void addCollaboration(PValue pv, JabicsUser u);
-
-	public void  deleteCollaboration(PValue pv, JabicsUser u);
+	public PValue createPValue(Property p, String s, Contact c, JabicsUser u);
 
 	public JabicsUser deleteCollaboration(Contact c, JabicsUser u);
 
 	public ContactList deleteCollaboration(ContactList cl, JabicsUser u);
 
-	public ArrayList<PValue> getPValueOf(Contact c, JabicsUser u);
+	public void  deleteCollaboration(PValue pv, JabicsUser u);
 
-	public ArrayList<Contact> getContactsOfList(ContactList cl, JabicsUser u);
+	public void deleteContact(Contact c, JabicsUser u);
+	
+	public ContactList deleteContactList(ContactList cl, JabicsUser u);
+	
+	public void deleteProperty(Property p);
 
-	public ArrayList<Contact> searchExpressionInList(String s, ContactList cl);
+	public void deletePValue(PValue pv, Contact c);
 
-	public ArrayList<Contact> searchInList(ContactList cl, PValue pv);
-
-//	public ArrayList<Contact> searchInList(int i, ContactList cl);
-//
-//	public ArrayList<Contact> searchInList(float f, ContactList cl);
-
-	public ArrayList<Contact> searchInList(JabicsUser u, ContactList cl);
-
-	public JabicsUser getOwnerOfContact(Contact c);
-
-	public ArrayList<JabicsUser> getCollaborators(Contact c);
-
-	public ArrayList<JabicsUser> getCollaborators(ContactList cl);
+	public void deleteUser(JabicsUser u);
 
 	public ArrayList<JabicsUser> getAllNotCollaboratingUser(Contact c);
 
 	public ArrayList<JabicsUser> getAllNotCollaboratingUser(ContactList cl);
 
-	public ArrayList<JabicsUser> getCollaborators(PValue pv);
+	public ArrayList<Contact> getAllSharedContactsOf(JabicsUser u);
 
 	public ArrayList<JabicsUser> getAllUsers();
 
+	public ArrayList<JabicsUser> getCollaborators(Contact c);
+
+	public ArrayList<JabicsUser> getCollaborators(ContactList cl);
+
+	public ArrayList<JabicsUser> getCollaborators(PValue pv);
+
+	public ArrayList<Contact> getContactsOf(JabicsUser u);
+
+	public ArrayList<Contact> getContactsOfList(ContactList cl, JabicsUser u);
+
+	public ArrayList<ContactList> getListsOf(JabicsUser u);
+
+	public JabicsUser getOwnerOfContact(Contact c);
+
+	public ArrayList<Property> getPropertysOfJabicsUser(JabicsUser u) throws IllegalArgumentException;
+
+	public ArrayList<PValue> getPValueOf(Contact c, JabicsUser u);
+
+//	public ArrayList<Contact> searchInList(int i, ContactList cl);
+//
+//	public ArrayList<Contact> searchInList(float f, ContactList cl);
+
 	public ArrayList<Property> getStandardProperties() throws IllegalArgumentException;
+
+	public Contact getUpdatedContact(Contact c);
 
 	public void initialise();
 
-	public ArrayList<Property> getPropertysOfJabicsUser(JabicsUser u) throws IllegalArgumentException;
+	public Contact removeContactFromList(Contact c, ContactList cl);
+
+	public ArrayList<Contact> searchExpressionInList(String s, ContactList cl);
+
+	public ArrayList<Contact> searchForContactByExpression(String s, JabicsUser u);
+
+	public ArrayList<Contact> searchInList(ContactList cl, PValue pv);
+
+	public ArrayList<Contact> searchInList(JabicsUser u, ContactList cl);
+
+	public Contact updateContact(Contact c, JabicsUser u);
+
+	public ContactList updateContactList(ContactList cl);
+
+	public PValue updatePValue(PValue pv);
 
 }
