d118f42f1ca6005c52cccb0e3c0647664290eea5
Noel Brase <nb086@users.noreply.github.com>
2018-07-05 19:39:07 +0200

    Auswahl im StackPanel ist nicht mehr gelb.
    untere StackPanel Auswahl geändert

--- ITProject_Group11_Jabics/src/de/hdm/group11/jabics/client/gui/ContactCellListTab.java
+++ ITProject_Group11_Jabics/src/de/hdm/group11/jabics/client/gui/ContactCellListTab.java
@@ -48,8 +48,10 @@ public class ContactCellListTab {
 	 *  Der Konstruktor ist mehrfach überladen damit auch ein TreeViewMenu mitgegebn werden kann.
 	 *  
 	 * @param u der Nutzer für den die Anzeige ausgegeben werden soll.
+	 * @param clRes2 
+	 * @param treeViewMenu2 
 	 */
-	public ContactCellListTab(JabicsUser u) {
+	public ContactCellListTab(JabicsUser u, TreeViewMenu treeViewMenu2, de.hdm.group11.jabics.client.gui.TreeViewMenu.CellListResources clRes2) {
 		
 		this.user = u;
 		
